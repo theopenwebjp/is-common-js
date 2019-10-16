8e43139422ab3b7d9a291077d888ccfa3b97b5b7
Damien Golding <noreply@damiengolding.com>
2019-10-16 23:32:23 +0900

    formatting and documentation

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 
 const traverse = require('traverse')
-const uglify = require("uglify-es")// require("uglify-js")
+const uglify = require('uglify-es')// require("uglify-js")
 const FIRST_INDEX = 0
 const SECOND_INDEX = 1
 
-class IsCommonJS{
-    static isCommonJS(src) {
-        let hasRequires = false
-        let hasExports = null
-        try{
-            hasRequires = IsCommonJS._hasRequire(src)
-            hasExports = src.match(/module.exports.\w+/) || src.match(/exports.\w+/) || src.match(/module.exports\s?=/)
-        }catch(error){
-            console.warn('handled error below so returning false')
-            console.error(error)
-        }
-        
-        return hasRequires || hasExports != null
+class IsCommonJS {
+  /**
+   * @param {string} src
+   * @return {boolean}
+   */
+  static isCommonJS (src) {
+    let hasRequires = false
+    let hasExports = null
+    try {
+      hasRequires = IsCommonJS._hasRequire(src)
+      hasExports = src.match(/module.exports.\w+/) || src.match(/exports.\w+/) || src.match(/module.exports\s?=/)
+    } catch (error) {
+      console.warn('handled error below so returning false')
+      console.error(error)
     }
 
-    static _hasRequire(src) {
-        const hasRequire = false
-        const ast = uglify.parse(src.toString())
+    return hasRequires || hasExports != null
+  }
 
-        traverse(ast).forEach(function(node) {
-            const potentialNode = Array.isArray(this.node) && this.node[FIRST_INDEX]
-            if(!potentialNode) return undefined;
+  /**
+   * @param {string} src
+   * @return {boolean}
+   */
+  static _hasRequire (src) {
+    let hasRequire = false
+    const ast = uglify.parse(src.toString())
 
-            if(IsCommonJS.isNamedCall(this.node, 'require')) {
-                hasRequire = true;
-            }
-        });
+    traverse(ast).forEach(function (node) {
+      const potentialNode = Array.isArray(node) && node[FIRST_INDEX]
+      if (!potentialNode) {
+        return undefined
+      }
 
-        return hasRequire;
-    }
+      if (IsCommonJS.isNamedCall(node, 'require')) {
+        hasRequire = true
+      }
+    })
 
-    static isNamedCall(node, word) {
-        const NODE_NAME_INDEX = 0
-        const NODE_VALUE_INDEX = 1
+    return hasRequire
+  }
 
-        const nodeName = node[NODE_NAME_INDEX];
-        const nodeValue = node.slice(NODE_VALUE_INDEX);
+  /**
+   * @param {Node} node
+   * @param {string} word
+   * @return {boolean}
+   */
+  static isNamedCall (node, word) {
+    const NODE_NAME_INDEX = 0
+    const NODE_VALUE_INDEX = 1
 
-        return nodeName === 'call'
-                && nodeValue[FIRST_INDEX][FIRST_INDEX] === 'name'
-                && nodeValue[FIRST_INDEX][SECOND_INDEX] === word;
-    }
+    const nodeName = node[NODE_NAME_INDEX]
+    const nodeValue = node.slice(NODE_VALUE_INDEX)
+
+    return nodeName === 'call' &&
+                nodeValue[FIRST_INDEX][FIRST_INDEX] === 'name' &&
+                nodeValue[FIRST_INDEX][SECOND_INDEX] === word
+  }
 }
 
 module.exports = IsCommonJS
